68a774a4985d178ee244d31269ee8cd9ddf38e66
Fuad Sahinovic | Strange Consult <fuad.sahinovic@strange-consult.de>
2020-02-24 15:55:22 +0100

    Initial commit

---
+++ src/com/company/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.company;
+import java.util.Scanner;
+public class HelloWorld {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello world");
+    }
+}
+
+
---
+++ src/com/company/Sabiranje.java
@@ -0,0 +1,36 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Sabiranje {
+
+    static boolean jelDjeljiv(int x) {
+        int a=x;
+        int i=0;
+        int cifra=0;
+        while(a!=0) {
+            cifra=a%10;
+            i+=cifra;
+            a/=10;
+        }
+        if(x%i==0 && x!=i)
+            return true;
+        else
+            return false;
+    }
+    public static void main(String[] args) {
+
+        int n;
+        Scanner ulaz= new Scanner(System.in);
+        System.out.println("Unesite zelejni broj ");
+        n= ulaz.nextInt();
+        System.out.println("Brojevi koji su djeljivi sa sumom svojih cifara su: ");
+        for(int i=1; i<n; i++) {
+            if(jelDjeljiv(i))
+                System.out.print(i+" ");
+                else
+            continue;
+        }
+
+    }
+}
